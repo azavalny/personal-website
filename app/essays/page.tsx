1eecc443a500e6dfcd96364977e0b08f766c9fb9
Alex Zavalny <thelazyaz@gmail.com>
2025-06-01 22:21:23 -0400

    essays under construction

--- app/essays/page.tsx
+++ app/essays/page.tsx
@@ -9,6 +9,7 @@ export default function Page() {
   return (
     <section>
       <h1 className="font-semibold text-2xl mb-8 tracking-tighter">Essays</h1>
+      <h2 className="font-semibold text-1xl mb-6 tracking-tighter">My insights on tech, finance, and life</h2>
       <BlogPosts />
     </section>
   )
